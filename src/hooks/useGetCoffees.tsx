44cf3753df03d7c159f30cee30d5eaaff14627bb
Pedro Oliveira <pedro_oliver13@hotmail.com>
2024-03-18 14:11:08 -0300

    feat: add useGetCoffees hook (#7)
    * feat: add useGetCoffees hook
    
    * fix: fix in prod

---
+++ src/hooks/useGetCoffees.tsx
@@ -0,0 +1,32 @@
+// Packages
+import { useState, useEffect } from 'react';
+
+interface Coffee {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+}
+
+interface UseGetCoffeesReturn {
+  data: Coffee[];
+  isLoading: boolean;
+}
+
+export const useGetCoffees = (): UseGetCoffeesReturn => {
+  const [data, setData] = useState<Coffee[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      'https://my-json-server.typicode.com/pedrooliver13/coffee-delivery-api/db',
+      { cache: 'force-cache' }
+    )
+      .then((results) => results.json())
+      .then((data) => setData(data?.coffees))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return { data, isLoading };
+};
