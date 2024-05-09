815f772d0d4e36ac59dc7856bfbab054031e6d7b
pedrooliver13 <pedro.oliveira@qyon.com>
2024-05-09 20:04:30 -0300

    feat: remove unsed hook

--- src/hooks/useCart.tsx
+++
@@ -1,7 +0,0 @@
-// Packages
-import { useContext } from 'react';
-
-// Contexts
-import { CartContext, CartContextProps } from 'contexts/cartContext';
-
-export const useCart = (): CartContextProps => useContext(CartContext);
